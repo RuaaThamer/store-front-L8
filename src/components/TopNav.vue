32a855d15ebd27f49dc0d6ccfc2704b41062d359
RuaaThamer <tham0022@algonquinlive.com>
2026-04-23 00:10:07 -0400

    done

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -2,7 +2,7 @@
   <nav>
     <div class="logo">
       <a href="/">
-        <img src="/algonquin-pet-store.png" alt="Algonquin Pet Store Logo">
+        <img src="/https://cdn.freebiesupply.com/logos/large/2x/best-buy-logo-black-and-white.png" alt="Algonquin Pet Store Logo">
       </a>
     </div>
     <button class="hamburger" @click="toggleNav">
